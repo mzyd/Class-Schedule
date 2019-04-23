f9d09187efcc43c7aaebdc420ae843a7f3d1a372
mzy <mzy3991@gmail.com>
2019-04-23 13:57:00 +0800

    乱七八糟

--- src/App.js
+++ src/App.js
@@ -2,29 +2,59 @@ import React, { Component } from 'react';
 import './App.css';
 import SiderBar from './App/SideBar.js'
 import Card from './App/Card.js'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { createBrowserHistory } from "history";
+const history = createBrowserHistory()
 
 
+const list = [
+  {
+    name: "李放假",
+    already: [
+      {
+        class: 1,
+        date: '2003-3-2',
+      }
+    ]
+  },
+  {
+    name: "积分就",
+    already: [
+      {
+        class: 1,
+        date: '2003-3-2',
+      }
+    ]
+  }
+
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          title
-        </header>
 
-        <div className="sider-bar-wrap clearfix">
-          <SiderBar />
+      <Router history={ history }>
+
+        <div className="App">
+          <header className="App-header">
+            title
+          </header>
+
+          <div className="sider-bar-wrap clearfix">
+            <SiderBar list={ list } />
+          </div>
+
+          <div className="content clearfix">
+            <Route path="/student/:id" component={ Card } />
+          </div>
+
+          <footer>
+            footer
+          </footer>
         </div>
 
-        <div className="content clearfix">
-          <Card />
-        </div>
+      </Router>
 
-        <footer>
-          footer
-        </footer>
-      </div>
     );
   }
 }
--- src/App/SideBar.js
+++ src/App/SideBar.js
@@ -1,37 +1,31 @@
 import React, { Component } from 'react'
 import './SiderBar.scss';
+import { Route, Link } from "react-router-dom";
+import Card from './Card.js'
+
+
+const NameItem = ({ props }) => {
+  console.log( "props : ", props )
+  return (
+    <li>
+      <Link to="/student/:id">{ props.name }</Link>
+    </li>
+  )
+}
 
 class SiderBar extends Component {
+
+  componentDidMount() {
+  }
+
   render() {
     return (
       <div className="sider-bar">
         <h3>学员</h3>
         <ul>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
-          <li>abc</li>
+          {this.props.list.map(item => (
+            <NameItem props={ item } />
+          ))}
         </ul>
       </div>
     )
